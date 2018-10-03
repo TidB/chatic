e27cf1bb67df2ff5f4b571a6054c8797c9bb3f82
TidB <tidb@gmx.net>
2018-10-03 16:34:03 +0200

    Added months, using d3-tips for tooltips. Updated data with new format on the master branch.

--- stats.js
+++ stats.js
@@ -69,7 +69,6 @@ function generateHours(data) {
 
 function generateYears(data) {
     const YEARS = d3.entries(data['years']);
-    console.log(YEARS)
     const MAX_YEAR = d3.max(YEARS, o => {return o.value.messages});
 
     var padding = 20;
@@ -132,13 +131,85 @@ function generateYears(data) {
     .text(function (b) {return d3.format(',')(b.value.messages);})
 }
 
-function generateMonths() {
-    var xScale = d3.scale()
-    .domain([new Date(), new Date()])
+function generateMonths(data) {
+    var MONTHS = [];
+    var skipped = false;
+    for (const [year, value] of Object.entries(data['months'])) {
+        for (const [i, month] of Object.entries(value)) {
+            if (!skipped && month.messages === 0) continue;
+
+            month.year = year;
+            month.month = Number(i) + 1;
+            MONTHS.push(month);
+            skipped = true;
+        }
+    }
+    const MAX_MONTH = d3.max(MONTHS, o => {return o.messages});
+
+    var padding = 20;
+
+    var width = 400;
+    var height = 150;
+
+    var totalWidth = width + padding * 2;
+    var totalHeight = height + padding * 2;
+
+    d3.select('#content')
+    .append('h1')
+    .attr('class', 'chart-title')
+    .text('Messages per month');
+
+    var svg = d3.select('#content')
+    .append('svg')
+    .attr('class', 'chart-months')
+    .attr('viewBox', `0 0 ${totalWidth} ${totalHeight}`);
+
+    shortDate = d3.timeFormat('%b \'%y');
+    tip = d3.tip()
+    .attr('class', 'd3-tip').html(function (b) {
+        return shortDate(new Date(b.year, b.month-1)) + '<br/>' + b.messages
+    })
+    .offset([-10, 0]);
+    svg.call(tip)
+
+    svg.selectAll('rect')
+    .data(MONTHS)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('width', function (b, i) {return width / MONTHS.length - 0.5})
+    .attr('height', function (b, i) {return (b.messages / MAX_MONTH) * height})
+    .attr('x', function (b, i) {return i * (width / MONTHS.length)})
+    .attr('y', function (b, i) {return height - (b.messages / MAX_MONTH) * height})
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
+
+    var xScale = d3.scaleTime()
+    .domain([
+        new Date(MONTHS[0].year, MONTHS[0].month-1),
+        new Date(MONTHS[MONTHS.length-1].year, MONTHS[MONTHS.length-1].month-1)
+    ])
+    .range([-0.5, width - 1]);
+    var xAxis = d3.axisBottom()
+    .scale(xScale)
+    .tickSizeOuter(0)
+    .tickSizeInner(2);
+
+    svg.append('g')
+    .attr('class', 'x-axis')
+    .attr('transform', 'translate(0, ' + height + ')')
+    .call(xAxis);
+
+    var yScale = d3.scaleLinear()
+    .domain([0, MAX_MONTH])
+    .range([0, height]);
+    var yAxis = d3.axisLeft()
+    .scale(yScale)
 }
 
 function main(data) {
     generateIntro(data);
+    generateMonths(data);
     generateYears(data);
     generateHours(data);
 }
